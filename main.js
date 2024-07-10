8f7c1426ac1846c9f476be251187f36bf2ef58b3
Woohyuk Byun <qusdngur1008@gmail.com>
2024-07-11 01:50:42 +0900

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-const API_KEY = `45db31da8f1e4247abfb1854c633e99b`;
-let news = []
+//const API_KEY = "45db31da8f1e4247abfb1854c633e99b";
+const API_KEY = `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`;
+const pageSize = 10
+let page = 2
+let category = ""
+let keyword = ""
+let newsList = []
+
+
 const getLatestNews = async () => {
     const url = new URL(
-        `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=${PAGE_SIZE}`
+
     );
     const response = await fetch(url) // 비동기함수
     const data = await response.json()
-    news = data.articles
-    console.log("dddd" , news)
+    newsList = data.articles
+    render()
+    console.log("dddd" , newsList)
 }
-getLatestNews()
+
+
+const render = () =>{
+    const newsHTML = newsList.map(
+        (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img class="news-img-size"
+              src=${news.urlToImage}
+              alt=""
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+          </div>
+        </div>`
+    ) 
+    
+
+    document.getElementById('news-board').innerHTML = newsHTML
+}
